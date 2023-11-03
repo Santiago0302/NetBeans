1efae88f6d14f041443e15cac0378cbfefd4bd74
Santiago0302 <149425309+Santiago0302@users.noreply.github.com>
2023-11-03 13:02:38 -0500

    Add files via upload

---
+++ Netbeanspractica1.java
@@ -0,0 +1,24 @@
+/* Carolina Santiago Hernandez 20-octubre-2023
+/* Practica-1 Netbeans
+/* El proposito de esta practica es mostrar un mensaje en pantalla.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netbeanspractica1;
+
+/**
+ *
+ * @author SANTIAGORP
+ */
+public class Netbeanspractica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Este es un mensaje en pantalla");
+        // TODO code application logic here
+    }
+    
+}
